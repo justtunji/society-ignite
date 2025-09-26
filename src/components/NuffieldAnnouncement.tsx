036780c18e6a51f877b610ca475528e6576e2cd1
justtunji <justmakinde@gmail.com>
2025-09-26 07:41:55 +0100

    Update NuffieldAnnouncement.tsx

--- src/components/NuffieldAnnouncement.tsx
+++ src/components/NuffieldAnnouncement.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { ExternalLink } from "lucide-react";
+import { PartnerSponsorDialog } from "./PartnerSponsorDialog";
 
 export const NuffieldAnnouncement = () => {
   return (
